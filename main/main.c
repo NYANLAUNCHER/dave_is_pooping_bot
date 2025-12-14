0c5da33d79b3fd30173c1a45931cb0041642bfea
markiep <purcellmarkie@proton.me>
2025-12-14 17:33:18 -0600

    update

--- main/main.c
+++ main/main.c
@@ -12,98 +12,51 @@
 #include "discord/message.h"
 #include "estr.h"
 
-#include "hx711.h"
+#include <hx711.h>
+
+#define CONFIG_EXAMPLE_PD_SCK_GPIO GPIO_NUM_36
+#define CONFIG_EXAMPLE_DOUT_GPIO GPIO_NUM_16
+#define CONFIG_EXAMPLE_AVG_TIMES 5
+
+#define TRUE 1
+#define FALSE 0
 
 static const char* DTAG = "discord_bot";
-static int32_t g_discord_event;
-static discord_event_data_t* g_discord_event_data;
+static int32_t g_discord_bot_connected=FALSE;
+static int32_t g_discord_event=0;
+static discord_event_data_t* g_discord_event_data=NULL;
 static discord_handle_t g_bot;
 
 static void bot_event_handler(void* handler_arg, esp_event_base_t base, int32_t event_id, void* event_data) {
-    static const char* TAG = "discord_bot";
-    g_discord_event = event_id
+    g_discord_event = event_id;
     g_discord_event_data = (discord_event_data_t*) event_data;
 
-    /*
-    discord_event_data_t* data = (discord_event_data_t*) event_data;
     switch(event_id) {
-        case DISCORD_EVENT_CONNECTED: {
-                discord_session_t* session = (discord_session_t*) data->ptr;
-
-                ESP_LOGI(TAG, "Bot %s#%s connected",
+        case DISCORD_EVENT_CONNECTED:
+            {
+                g_discord_bot_connected=TRUE;
+                discord_session_t* session = (discord_session_t*) g_discord_event_data->ptr;
+                ESP_LOGI(DTAG, "Bot %s#%s connected",
                     session->user->username,
                     session->user->discriminator
                 );
             }
             break;
-        
-        case DISCORD_EVENT_MESSAGE_RECEIVED: {
-                discord_message_t* msg = (discord_message_t*) data->ptr;
-
-                ESP_LOGI(TAG, "New message (dm=%s, autor=%s#%s, bot=%s, channel=%s, guild=%s, content=%s)",
-                    ! msg->guild_id ? "true" : "false",
-                    msg->author->username,
-                    msg->author->discriminator,
-                    msg->author->bot ? "true" : "false",
-                    msg->channel_id,
-                    msg->guild_id ? msg->guild_id : "NULL",
-                    msg->content
-                );
-
-                char* echo_content = estr_cat("Hey ", msg->author->username, " you wrote `", msg->content, "`");
-
-                discord_message_t echo = {
-                    .content = echo_content,
-                    .channel_id = msg->channel_id
-                };
-
-                discord_message_t* sent_msg = NULL;
-                esp_err_t err = discord_message_send(g_bot, &echo, &sent_msg);
-                free(echo_content);
-
-                if(err == ESP_OK) {
-                    ESP_LOGI(TAG, "Echo message successfully sent");
-
-                    if(sent_msg) { // null check because message can be sent but not returned
-                        ESP_LOGI(TAG, "Echo message got ID #%s", sent_msg->id);
-                        discord_message_free(sent_msg);
-                    }
-                } else {
-                    ESP_LOGE(TAG, "Fail to send echo message");
-                }
-            }
-            break;
-        
-        case DISCORD_EVENT_MESSAGE_UPDATED: {
-                discord_message_t* msg = (discord_message_t*) data->ptr;
-                ESP_LOGI(TAG, "%s has updated his message (#%s). New content: %s", msg->author->username, msg->id, msg->content);
-            }
-            break;
-        
-        case DISCORD_EVENT_MESSAGE_DELETED: {
-                discord_message_t* msg = (discord_message_t*) data->ptr;
-                ESP_LOGI(TAG, "Message #%s deleted", msg->id);
-            }
-            break;
-        
         case DISCORD_EVENT_DISCONNECTED:
-            ESP_LOGW(TAG, "Bot logged out");
+            ESP_LOGW(DTAG, "Bot logged out");
             break;
     }
-    */
 }
 
-void hx711_event_handler(void *pvParameters) {
+static void hx711_event_handler(void *pvParameters) {
     static const char* TAG = "hx711";
     const hx711_t dev = {
         .dout = CONFIG_EXAMPLE_DOUT_GPIO,
         .pd_sck = CONFIG_EXAMPLE_PD_SCK_GPIO,
         .gain = HX711_GAIN_A_64
     };
-
     // initialize device
     ESP_ERROR_CHECK(hx711_init(&dev));
-
     // read from device
     while (1) {
         esp_err_t r = hx711_wait(&dev, 500);
@@ -117,30 +70,36 @@ void hx711_event_handler(void *pvParameters) {
             ESP_LOGE(TAG, "Could not read data: %d (%s)\n", r, esp_err_to_name(r));
             continue;
         }
+        char str_load_data[50];
+        sprintf(str_load_data, "%ld", load_data);
         // debug data values using idf.py monitor
-        ESP_LOGI(TAG, "Raw data: %" PRIi32, data);
+        ESP_LOGI(TAG, "Raw data: %s", str_load_data);
 
-        discord_message_t* msg = (discord_message_t*) g_discord_event_data->ptr;
-        char* echo_content = estr_cat("Raw load cell data: ", load_data);
-        discord_message_t echo = {
-            .content = echo_content,
-            .channel_id = msg->channel_id
-        };
-        discord_message_t* sent_msg = NULL;
-        esp_err_t err = discord_message_send(g_bot, &echo, &sent_msg);
-        free(echo_content);
+        if (g_discord_bot_connected) {
+            discord_message_t* msg = (discord_message_t*) g_discord_event_data->ptr;
+            char* echo_content = estr_cat("Raw load cell data: ", str_load_data);
+            discord_message_t echo = {
+                .content = echo_content,
+                .channel_id = msg->channel_id
+            };
+            discord_message_t* sent_msg = NULL;
+            esp_err_t err = discord_message_send(g_bot, &echo, &sent_msg);
+            free(echo_content);
 
-        if(err == ESP_OK) {
-            ESP_LOGI(TAG, "Echo message successfully sent");
+            if(err == ESP_OK) {
+                ESP_LOGI(DTAG, "Echo message successfully sent");
 
-            if(sent_msg) { // null check because message can be sent but not returned
-                ESP_LOGI(TAG, "Echo message got ID #%s", sent_msg->id);
-                discord_message_free(sent_msg);
+                if(sent_msg) { // null check because message can be sent but not returned
+                    ESP_LOGI(DTAG, "Echo message got ID #%s", sent_msg->id);
+                    discord_message_free(sent_msg);
+                }
+            } else {
+                ESP_LOGE(DTAG, "Failed to send echo message");
             }
         } else {
-            ESP_LOGE(TAG, "Failed to send echo message");
+            ESP_LOGI(TAG, "Waiting for discord bot to connect.");
         }
-        vTaskDelay(pdMS_TO_TICKS(500));
+        vTaskDelay(pdMS_TO_TICKS(2000));
     }
 }
 
